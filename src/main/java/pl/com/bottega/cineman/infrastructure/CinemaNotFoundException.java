cd8892b6664e315165752bf84e54efe3c138062b
bartekma89 <bartekma89@gmail.com>
2017-04-09 12:47:36 +0200

    JPACinemaRepository update

--- src/main/java/pl/com/bottega/cineman/infrastructure/CinemaNotFoundException.java
+++ src/main/java/pl/com/bottega/cineman/infrastructure/CinemaNotFoundException.java
@@ -1,1 +1,1 @@
-package pl.com.bottega.cineman.infrastructure;/** * Created by Bartek on 2017-04-09. */public class CinemaNotFoundException extends RuntimeException {	public CinemaNotFoundException(Long id) {		super(String.format("Cinema with id %s does not exist", id));	}}
+package pl.com.bottega.cineman.infrastructure;/** * Created by Bartek on 2017-04-09. */public class CinemaNotFoundException extends RuntimeException {	public CinemaNotFoundException(Long id) {		super(String.format("Cinema with id %s does not exist ", id));	}}
