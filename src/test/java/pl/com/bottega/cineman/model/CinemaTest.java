b283a33e91c9909f39f668095fd0ea148950f57f
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-04-09 22:38:11 +0200

    unit tests for cinema

--- src/test/java/pl/com/bottega/cineman/model/CinemaTest.java
+++ src/test/java/pl/com/bottega/cineman/model/CinemaTest.java
@@ -1,12 +1,42 @@
 package pl.com.bottega.cineman.model;
 
 import org.junit.Test;
+import pl.com.bottega.cineman.model.commands.CreateCinemaCommand;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CinemaTest {
 
-	@Test
-	public void shouldAddCinema() {
+	private String city;
+	private String name;
 
+	@Test
+	public void shouldRememberName() {
+		//given
+		name = "Felicity";
+		//when
+		Cinema cinema = createCinema();
+		//then
+		assertThat(cinema.getName()).isEqualTo(name);
+	}
+
+	@Test
+	public void shouldRememberCity() {
+		//given
+		city = "Lublin";
+		name = "Felicity";
+		//when
+		Cinema cinema = createCinema();
+		//then
+		assertThat(cinema.getName()).isEqualTo(name);
+		assertThat(cinema.getCity()).isEqualTo(city);
+	}
+
+	private Cinema createCinema() {
+		CreateCinemaCommand command = new CreateCinemaCommand();
+		command.setName(name);
+		command.setCity(city);
+		return new Cinema(command);
 	}
 
 }
