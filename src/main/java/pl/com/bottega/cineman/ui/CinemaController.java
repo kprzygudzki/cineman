6fb87a39e31299d15411ef009df9165458488be8
anna <krzysztoszek.a@gmail.com>
2017-04-09 10:50:18 +0200

    add ui classes

--- src/main/java/pl/com/bottega/cineman/ui/CinemaController.java
+++ src/main/java/pl/com/bottega/cineman/ui/CinemaController.java
@@ -1,4 +1,25 @@
 package pl.com.bottega.cineman.ui;
 
+import pl.com.bottega.cineman.application.AdminPanel;
+import pl.com.bottega.cineman.application.CinemaDto;
+import pl.com.bottega.cineman.model.commands.CreateCinemaCommand;
+import pl.com.bottega.cineman.model.commands.CreateShowingsCommand;
+
+import java.util.List;
+
 public class CinemaController {
+
+    private AdminPanel adminPanel;
+
+    void create (CreateCinemaCommand cmd) {
+
+    }
+
+    List<CinemaDto> showAll() {
+        return null;
+    }
+
+    void createShowings(CreateShowingsCommand cmd) {
+
+    }
 }
---
+++ src/main/java/pl/com/bottega/cineman/ui/MovieController.java
@@ -0,0 +1,14 @@
+package pl.com.bottega.cineman.ui;
+
+import pl.com.bottega.cineman.application.AdminPanel;
+import pl.com.bottega.cineman.model.commands.CreateMovieCommand;
+
+public class MovieController {
+
+    private AdminPanel adminPanel;
+
+    void create(CreateMovieCommand cmd) {
+
+    }
+
+}
