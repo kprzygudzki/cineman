4f5b5c8f0d8d3640fa62c90f18aee0efe2fa31c1
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-04-13 13:04:53 +0200

    polishing up

--- src/test/java/pl/com/bottega/cineman/acceptance/AdminPanelTest.java
+++ src/test/java/pl/com/bottega/cineman/acceptance/AdminPanelTest.java
@@ -1,6 +1,5 @@
 package pl.com.bottega.cineman.acceptance;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
