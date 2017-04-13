c95bec498eeb30806e255b2bf2f84c80d03c45fe
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-04-14 00:23:59 +0200

    reworked JPACinemaCatalog to use CriteriaAPI instead of JPQL

--- src/main/java/pl/com/bottega/cineman/infrastructure/JPACinemaCatalog.java
+++ src/main/java/pl/com/bottega/cineman/infrastructure/JPACinemaCatalog.java
@@ -7,7 +7,8 @@ import pl.com.bottega.cineman.model.Cinema;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,11 +19,17 @@ public class JPACinemaCatalog implements CinemaCatalog {
 
 	@Override
 	public List<CinemaDto> getCinemas() {
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Cinema> criteria = builder.createQuery(Cinema.class);
+		criteria.from(Cinema.class);
+		List<Cinema> cinemas = entityManager.createQuery(criteria).getResultList();
+		return createCinemaDtos(cinemas);
+	}
+
+	private List<CinemaDto> createCinemaDtos(List<Cinema> cinemas) {
 		List<CinemaDto> cinemaDtos = new ArrayList<>();
-		Query query = entityManager.createQuery("FROM Cinema");
-		List<Cinema> cinemas = query.getResultList();
+		CinemaDtoBuilder builder = new CinemaDtoBuilder();
 		for (Cinema cinema : cinemas) {
-			CinemaDtoBuilder builder = new CinemaDtoBuilder();
 			cinema.export(builder);
 			CinemaDto cinemaDto = builder.build();
 			cinemaDtos.add(cinemaDto);
