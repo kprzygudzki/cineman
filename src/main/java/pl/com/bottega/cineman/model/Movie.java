e0dd1dd9064461b37a6b91e4449368e0669f454a
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-04-23 22:19:01 +0200

    added equals and hashCode to Movie

--- src/main/java/pl/com/bottega/cineman/model/Movie.java
+++ src/main/java/pl/com/bottega/cineman/model/Movie.java
@@ -69,4 +69,19 @@ public class Movie {
 		return pricing;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Movie movie = (Movie) o;
+
+		return id != null ? id.equals(movie.id) : movie.id == null;
+	}
+
+	@Override
+	public int hashCode() {
+		return id != null ? id.hashCode() : 0;
+	}
+
 }
