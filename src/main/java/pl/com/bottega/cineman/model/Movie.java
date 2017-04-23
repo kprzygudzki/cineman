12c982c3d59fe91266bb7734df5148d38389f4fa
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-04-23 20:05:15 +0200

    removied unnecessary setters and getters

--- src/main/java/pl/com/bottega/cineman/model/Movie.java
+++ src/main/java/pl/com/bottega/cineman/model/Movie.java
@@ -61,34 +61,6 @@ public class Movie {
 		exporter.addLength(length);
 	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public void setActors(Set<String> actors) {
-		this.actors = actors;
-	}
-
-	public void setGenres(Set<String> genres) {
-		this.genres = genres;
-	}
-
-	public void setMinAge(Integer minAge) {
-		this.minAge = minAge;
-	}
-
-	public void setLength(Integer length) {
-		this.length = length;
-	}
-
 	public Long getId() {
 		return id;
 	}
