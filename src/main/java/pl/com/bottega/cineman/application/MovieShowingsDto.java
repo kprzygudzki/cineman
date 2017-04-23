57cf71c2df04e2d5877902b12049634c3a9386a0
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-04-23 22:20:39 +0200

    reworked MovieShowingsDto according to the new requirements

--- src/main/java/pl/com/bottega/cineman/application/MovieShowingsDto.java
+++ src/main/java/pl/com/bottega/cineman/application/MovieShowingsDto.java
@@ -1,72 +1,26 @@
 package pl.com.bottega.cineman.application;
 
 import java.util.List;
-import java.util.Set;
 
 public class MovieShowingsDto {
 
-	private String title;
-	private String description;
-	private Set<String> actors;
-	private Set<String> genres;
-	private Integer minAge;
-	private Integer length;
+	private MovieDto movie;
 	private List<ShowingDto> shows;
 
-	public String getTitle() {
-		return title;
+	public void setMovie(MovieDto movie) {
+		this.movie = movie;
 	}
 
-	void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Set<String> getActors() {
-		return actors;
-	}
-
-	void setActors(Set<String> actors) {
-		this.actors = actors;
-	}
-
-	public Set<String> getGenres() {
-		return genres;
-	}
-
-	void setGenres(Set<String> genres) {
-		this.genres = genres;
-	}
-
-	public Integer getMinAge() {
-		return minAge;
-	}
-
-	void setMinAge(Integer minAge) {
-		this.minAge = minAge;
-	}
-
-	public Integer getLength() {
-		return length;
-	}
-
-	void setLength(Integer length) {
-		this.length = length;
-	}
-
-	public List<ShowingDto> getShows() {
-		return shows;
+	public MovieDto getMovie() {
+		return movie;
 	}
 
 	public void setShows(List<ShowingDto> shows) {
 		this.shows = shows;
+	}
+
+	public List<ShowingDto> getShows() {
+		return shows;
 	}
 
 }
