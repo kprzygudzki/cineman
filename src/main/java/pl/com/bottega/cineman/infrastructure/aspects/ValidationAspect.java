ac9d7002f507a016e3b9b38dad9589484eefe5f9
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-05-06 09:02:00 +0200

    introduced validation aspect

---
+++ src/main/java/pl/com/bottega/cineman/infrastructure/aspects/ValidationAspect.java
@@ -0,0 +1,30 @@
+package pl.com.bottega.cineman.infrastructure.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import pl.com.bottega.cineman.model.commands.InvalidCommandException;
+import pl.com.bottega.cineman.model.commands.Validatable;
+import pl.com.bottega.cineman.model.commands.Validatable.ValidationErrors;
+
+@Component
+@Aspect
+public class ValidationAspect {
+
+	@Before( "execution(* pl.com.bottega.cineman.application..*.*(..)) && args(validatable,..)" )
+	public void validate(Validatable validatable) {
+		ValidationErrors errors = new ValidationErrors();
+		validatable.validate(errors);
+		if (!errors.isValid())
+			throw new InvalidCommandException(errors);
+	}
+
+//	@Before( "within(pl.com.bottega.cineman.application.*) && args(validatable,..)" )
+//	public void validate(Validatable validatable) {
+//		ValidationErrors errors = new ValidationErrors();
+//		validatable.validate(errors);
+//		if (!errors.isValid())
+//			throw new InvalidCommandException(errors);
+//	}
+
+}
