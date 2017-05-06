fa3d3da6f00a80f4b0621d9d7814e6c2dceac5fc
kprzygudzki <krzysztofprzygudzki@gmail.com>
2017-05-06 21:16:50 +0200

    improved validation

--- src/main/java/pl/com/bottega/cineman/model/commands/CreateReservationCommand.java
+++ src/main/java/pl/com/bottega/cineman/model/commands/CreateReservationCommand.java
@@ -9,6 +9,11 @@ import java.util.Set;
 
 public class CreateReservationCommand implements Validatable {
 
+	private static final String REQUIRED_FIELD = "is a required field and can not be empty";
+	private static final String AT_LEAST_ONE = "must at least be 1";
+	private static final String NOT_MORE_THAN = "must be no bigger than %s";
+	private static final String UNIQUE_TICKET_KINDS = "must contain unique ticket kinds";
+
 	private Long showId;
 	private Set<ReservationItem> tickets;
 	private Set<Seat> seats;
@@ -51,23 +56,83 @@ public class CreateReservationCommand implements Validatable {
 
 	@Override
 	public void validate(ValidationErrors errors) {
-		seats.remove(null);
-		if (seats == null || seats.isEmpty())
-			errors.add("seats", "is a required field and can not be empty");
-		tickets.remove(null);
-		if (tickets == null || tickets.isEmpty())
-			errors.add("tickets", "is a required field and can not be empty");
+		if (seats == null)
+			errors.add("seats", REQUIRED_FIELD);
 		else
-			ensureUniqueTicketKinds(errors);
+			validateSeats(errors);
+		if (tickets == null)
+			errors.add("tickets", REQUIRED_FIELD);
+		else
+			validateTickets(errors);
 		customer.validate(errors);
+	}
+
+	private void validateSeats(ValidationErrors errors) {
+		seats.remove(null);
+		if (seats.isEmpty())
+			errors.add("seats", REQUIRED_FIELD);
+		for (Seat seat : seats) {
+			validateRow(errors, seat);
+			validateSeat(errors, seat);
+		}
+	}
+
+	private void validateRow(ValidationErrors errors, Seat seat) {
+		Integer row = seat.getRow();
+		if (row == null)
+			errors.add("seat row", REQUIRED_FIELD);
+		else {
+			if (row.compareTo(1) < 0)
+				errors.add("seat row", AT_LEAST_ONE);
+			if (row.compareTo(10) > 0)
+				errors.add("seat row", String.format(NOT_MORE_THAN, 10));
+		}
+	}
+
+	private void validateSeat(ValidationErrors errors, Seat seat) {
+		Integer row = seat.getSeat();
+		if (row == null)
+			errors.add("seat number", REQUIRED_FIELD);
+		else {
+			if (row.compareTo(1) < 0)
+				errors.add("seat number", AT_LEAST_ONE);
+			if (row.compareTo(15) > 0)
+				errors.add("seat number", String.format(NOT_MORE_THAN, 15));
+		}
+	}
+
+	private void validateTickets(ValidationErrors errors) {
+		tickets.remove(null);
+		if (tickets.isEmpty())
+			errors.add("tickets", REQUIRED_FIELD);
+		for (ReservationItem ticket : tickets) {
+			ensureTicketKindPresent(errors, ticket);
+			ensureTicketCountPresent(errors, ticket);
+			ensureTicketCountBiggerThenZero(errors, ticket);
+		}
+		ensureUniqueTicketKinds(errors);
+	}
+
+	private void ensureTicketCountBiggerThenZero(ValidationErrors errors, ReservationItem ticket) {
+		if (ticket.getCount().compareTo(1) < 0)
+			errors.add("ticket count", AT_LEAST_ONE);
+	}
+
+	private void ensureTicketCountPresent(ValidationErrors errors, ReservationItem ticket) {
+		if (ticket.getCount() == null)
+			errors.add("ticket count", REQUIRED_FIELD);
+	}
+
+	private void ensureTicketKindPresent(ValidationErrors errors, ReservationItem ticket) {
+		if (ticket.getKind() == null || ticket.getKind().trim().isEmpty())
+			errors.add("ticket kind", REQUIRED_FIELD);
 	}
 
 	private void ensureUniqueTicketKinds(ValidationErrors errors) {
 		Set<String> kinds = new HashSet<>();
 		for (ReservationItem ticket : tickets)
 			if (!kinds.add(ticket.getKind()))
-				errors.add("tickets",
-						"can not contain multiple ticket positions of the same kind");
+				errors.add("tickets", UNIQUE_TICKET_KINDS);
 	}
 
 }
